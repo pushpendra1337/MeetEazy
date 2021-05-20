3976ce9564ef6ebba4af6b31f0e3b866d49b88e9
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-20 20:49:25 +0530

    Create Extras.java class to hold values of intent extras

---
+++ app/src/main/java/net/intensecorp/meeteazy/utils/Extras.java
@@ -0,0 +1,6 @@
+package net.intensecorp.meeteazy.utils;
+
+public class Extras {
+    public static final String EXTRA_FIRST_NAME = "net.intensecorp.meeeteazy.extras.extra.FIRST_NAME";
+    public static final String EXTRA_EMAIL = "net.intensecorp.meeeteazy.extras.extra.EMAIL";
+}
