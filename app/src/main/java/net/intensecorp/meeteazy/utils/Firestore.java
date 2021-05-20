80cfb8ad9b07e9a823c295d3151d4119cc034b5e
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-20 23:34:31 +0530

    Create Firestore.java class to hold constant values of Firestore collections and fields

---
+++ app/src/main/java/net/intensecorp/meeteazy/utils/Firestore.java
@@ -0,0 +1,22 @@
+package net.intensecorp.meeteazy.utils;
+
+public class Firestore {
+
+    public static final String COLLECTION_USERS = "users";
+
+    public static final String FIELD_FIRST_NAME = "first_name";
+
+    public static final String FIELD_LAST_NAME = "last_name";
+
+    public static final String FIELD_EMAIL = "email";
+
+    public static final String FIELD_ABOUT = "about";
+
+    public static final String FIELD_PROFILE_PICTURE_URL = "profile_picture_url";
+
+    public static final String FIELD_FCM_TOKEN = "fcm_token";
+
+    public static final String DEFAULT_VALUE_ABOUT = "Hey there! I am using MeatEazy.";
+
+    public static final String DEFAULT_VALUE_PROFILE_PICTURE_URL = "null";
+}
