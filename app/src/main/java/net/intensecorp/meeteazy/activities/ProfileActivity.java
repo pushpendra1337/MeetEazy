d0590fed5b07f09cd8acb1cbaa22f8bc17e1ae73
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-21 21:41:53 +0530

    Generate empty Profile activity

---
+++ app/src/main/java/net/intensecorp/meeteazy/activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package net.intensecorp.meeteazy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.intensecorp.meeteazy.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
