e288b044cc099c1da9c1cf8c4649991ab9f6b70c
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-21 20:53:49 +0530

    Create methods to retrieve and upload FCM Token to database

--- app/src/main/java/net/intensecorp/meeteazy/activities/HomeActivity.java
+++ app/src/main/java/net/intensecorp/meeteazy/activities/HomeActivity.java
@@ -1,6 +1,7 @@
 package net.intensecorp.meeteazy.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,12 +10,23 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import net.intensecorp.meeteazy.R;
+import net.intensecorp.meeteazy.utils.Firestore;
+
+import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,12 @@ public class HomeActivity extends AppCompatActivity {
 
         toolbar.setTitle(R.string.toolbar_title_search_in_rooms);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (isUserValid()) {
+            getFcmToken();
+        }
 
         toolbar.setNavigationOnClickListener(v -> Toast.makeText(HomeActivity.this, "Search", Toast.LENGTH_SHORT).show());
 
@@ -43,5 +61,30 @@ public class HomeActivity extends AppCompatActivity {
         profilePicture.setOnClickListener(v -> Toast.makeText(HomeActivity.this, "Profile", Toast.LENGTH_SHORT).show());
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private boolean isUserValid() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        return user != null && user.isEmailVerified();
+    }
+
+    private void getFcmToken() {
+        FirebaseMessaging.getInstance()
+                .getToken()
+                .addOnSuccessListener(s -> {
+                    Log.d(TAG, "FCM token: " + s);
+
+                    setFcmToken(s);
+                })
+                .addOnFailureListener(e -> Log.e(TAG, "Failed to get token: " + e.getMessage()));
+    }
+
+    private void setFcmToken(String token) {
+        FirebaseFirestore store = FirebaseFirestore.getInstance();
+        String uid = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+        DocumentReference userReference = store.collection(Firestore.COLLECTION_USERS).document(uid);
+        userReference.update(Firestore.FIELD_FCM_TOKEN, token)
+                .addOnSuccessListener(aVoid -> Log.d(TAG, "Token successfully updated: " + token))
+                .addOnFailureListener(e -> Log.e(TAG, "Failed to update token: " + e.getMessage()));
     }
 }
