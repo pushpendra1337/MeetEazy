7598a725ff805cc8b86158360288aa64860c37b0
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-21 21:43:35 +0530

    Generate empty About activity

---
+++ app/src/main/java/net/intensecorp/meeteazy/activities/AboutActivity.java
@@ -0,0 +1,16 @@
+package net.intensecorp.meeteazy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.intensecorp.meeteazy.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
