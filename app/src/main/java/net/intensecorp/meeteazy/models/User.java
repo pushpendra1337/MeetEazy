c0bbfda1300c5cf4dfa804b66802c2594474f90b
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-23 11:22:35 +0530

    Create User model class

---
+++ app/src/main/java/net/intensecorp/meeteazy/models/User.java
@@ -0,0 +1,12 @@
+package net.intensecorp.meeteazy.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String mFirstName;
+    public String mLastName;
+    public String mEmail;
+    public String mAbout;
+    public String mProfilePictureUrl;
+    public String mFcmToken;
+}
