8d6c97f69e6c89b8cac92800acae6d425ec3f47f
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-25 20:56:52 +0530

    Update Extras.java

--- app/src/main/java/net/intensecorp/meeteazy/utils/Extras.java
+++ app/src/main/java/net/intensecorp/meeteazy/utils/Extras.java
@@ -1,6 +1,8 @@
 package net.intensecorp.meeteazy.utils;
 
 public class Extras {
-    public static final String EXTRA_FIRST_NAME = "net.intensecorp.meeeteazy.extras.extra.FIRST_NAME";
-    public static final String EXTRA_EMAIL = "net.intensecorp.meeeteazy.extras.extra.EMAIL";
+    public static final String EXTRA_FIRST_NAME = "net.intensecorp.meeeteazy.utils.extra.FIRST_NAME";
+    public static final String EXTRA_EMAIL = "net.intensecorp.meeeteazy.utils.extra.EMAIL";
+    public static final String EXTRA_CALL_TYPE = "net.intensecorp.meeeteazy.utils.extra.CALL_TYPE";
+    public static final String EXTRA_CALLEE = "net.intensecorp.meeeteazy.utils.extra.CALLEE";
 }
