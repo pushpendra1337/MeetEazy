dde3f470fba030131347dd78710151cce05a0559
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-05-26 21:22:23 +0530

    Create methods to craft and send Call Response Messages

--- app/src/main/java/net/intensecorp/meeteazy/activities/IncomingCallActivity.java
+++ app/src/main/java/net/intensecorp/meeteazy/activities/IncomingCallActivity.java
@@ -2,9 +2,10 @@ package net.intensecorp.meeteazy.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -12,13 +13,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textview.MaterialTextView;
 
 import net.intensecorp.meeteazy.R;
+import net.intensecorp.meeteazy.api.ApiClient;
+import net.intensecorp.meeteazy.api.ApiService;
 import net.intensecorp.meeteazy.utils.ApiUtility;
 import net.intensecorp.meeteazy.utils.Extras;
 import net.intensecorp.meeteazy.utils.FormatterUtility;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class IncomingCallActivity extends AppCompatActivity {
+
+    private static final String TAG = IncomingCallActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +50,25 @@ public class IncomingCallActivity extends AppCompatActivity {
         String callerLastName = incomingCallIntent.getStringExtra(Extras.EXTRA_CALLER_LAST_NAME);
         String callerEmail = incomingCallIntent.getStringExtra(Extras.EXTRA_CALLER_EMAIL);
         String callerProfilePictureUrl = incomingCallIntent.getStringExtra(Extras.EXTRA_CALLER_PROFILE_PICTURE_URL);
+        String callerFcmToken = incomingCallIntent.getStringExtra(Extras.EXTRA_CALLER_FCM_TOKEN);
 
         switch (incomingCallType) {
-            case ApiUtility.TYPE_VOICE_CALL:
+            case ApiUtility.CALL_TYPE_VOICE:
                 incomingCallTypeIconView.setImageResource(R.drawable.ic_baseline_mic_24);
                 incomingCallTypeView.setText(R.string.text_incoming_call_type_voice);
                 break;
-            case ApiUtility.TYPE_VIDEO_CALL:
+
+            case ApiUtility.CALL_TYPE_VIDEO:
                 incomingCallTypeIconView.setImageResource(R.drawable.ic_baseline_videocam_24);
                 incomingCallTypeView.setText(R.string.text_incoming_call_type_video);
                 break;
+
+            default:
+                Log.e(TAG, "Incoming Call type unknown.");
+                break;
         }
 
-        if (!callerProfilePictureUrl.equals("null")) {
-
+        if (callerProfilePictureUrl != null) {
             Glide.with(IncomingCallActivity.this)
                     .load(callerProfilePictureUrl)
                     .centerCrop()
@@ -66,8 +82,65 @@ public class IncomingCallActivity extends AppCompatActivity {
 
         callerEmailView.setText(callerEmail);
 
-        rejectCallButton.setOnClickListener(v -> finish());
+        rejectCallButton.setOnClickListener(v -> craftCallResponseMessageBody(callerFcmToken, ApiUtility.RESPONSE_TYPE_REJECTED));
 
-        answerCallButton.setOnClickListener(v -> Toast.makeText(IncomingCallActivity.this, "Call answered", Toast.LENGTH_SHORT).show());
+        answerCallButton.setOnClickListener(v -> craftCallResponseMessageBody(callerFcmToken, ApiUtility.RESPONSE_TYPE_ANSWERED));
+    }
+
+    public void craftCallResponseMessageBody(String callerFcmToken, String responseType) {
+
+        try {
+            JSONArray callerFcmTokensArray = new JSONArray();
+            callerFcmTokensArray.put(callerFcmToken);
+
+            JSONObject body = new JSONObject();
+            JSONObject data = new JSONObject();
+
+            data.put(ApiUtility.KEY_MESSAGE_TYPE, ApiUtility.MESSAGE_TYPE_CALL_RESPONSE);
+            data.put(ApiUtility.KEY_RESPONSE_TYPE, responseType);
+
+            body.put(ApiUtility.JSON_OBJECT_DATA, data);
+            body.put(ApiUtility.JSON_OBJECT_REGISTRATION_IDS, callerFcmTokensArray);
+
+            sendCallResponseMessage(body.toString(), responseType);
+
+        } catch (Exception exception) {
+            Log.d(TAG, "Message can't be crafted: " + exception.getMessage());
+        }
+    }
+
+    private void sendCallResponseMessage(String messageBody, String responseType) {
+        ApiClient.getClient()
+                .create(ApiService.class)
+                .sendRemoteMessage(ApiUtility.getMessageHeaders(), messageBody)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                        if (response.isSuccessful()) {
+                            switch (responseType) {
+                                case ApiUtility.RESPONSE_TYPE_ANSWERED:
+                                    Log.d(TAG, "Call answered response message sent successfully.");
+                                    break;
+
+                                case ApiUtility.RESPONSE_TYPE_REJECTED:
+                                    Log.d(TAG, "Call rejected response message sent successfully.");
+                                    break;
+
+                                default:
+                                    Log.e(TAG, "Unknown response");
+                                    break;
+                            }
+                        } else {
+                            Log.d(TAG, "Response of sent message: " + response.message());
+                        }
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+                        Log.e(TAG, "Message not sent: " + t.getMessage());
+                        finish();
+                    }
+                });
     }
 }
