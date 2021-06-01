0f34c4c86798f40e0aa991863d4070955c5b6369
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-06-01 22:14:01 +0530

    Remove isOnline method which is slowing down the app

--- app/src/main/java/net/intensecorp/meeteazy/utils/NetworkInfoUtility.java
+++ app/src/main/java/net/intensecorp/meeteazy/utils/NetworkInfoUtility.java
@@ -4,14 +4,9 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
-import android.util.Log;
-
-import java.io.IOException;
 
 public class NetworkInfoUtility {
 
-    private static final String COMMAND_PING_GOOGLE = "/system/bin/ping -w 1 -c 1 8.8.8.8";
-    private static final String TAG = NetworkInfoUtility.class.getSimpleName();
     private final Context mContext;
 
     public NetworkInfoUtility(Context context) {
@@ -28,24 +23,8 @@ public class NetworkInfoUtility {
             if (network != null) {
                 NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
 
-                if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-                    return isOnline();
-                }
+                return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
             }
-        }
-
-        return false;
-    }
-
-    private boolean isOnline() {
-        try {
-            // Ping to Google server
-            Process ipProcess = Runtime.getRuntime().exec(COMMAND_PING_GOOGLE);
-            int exitValue = ipProcess.waitFor();
-            return (exitValue == 0);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            Log.e(TAG, "Ping failed: " + e.getMessage());
         }
 
         return false;
