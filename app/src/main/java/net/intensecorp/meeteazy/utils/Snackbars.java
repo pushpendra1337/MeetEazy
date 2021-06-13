3d9b7b4e2566af76300027c704a6d3ba7b67ea78
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-06-13 19:00:47 +0530

    Add feature to set anchor view to Snackbars

--- app/src/main/java/net/intensecorp/meeteazy/utils/Snackbars.java
+++ app/src/main/java/net/intensecorp/meeteazy/utils/Snackbars.java
@@ -19,7 +19,6 @@ import net.intensecorp.meeteazy.R;
 public class Snackbars {
 
     private final Activity mActivity;
-    private Snackbar mSnackbar;
 
     public Snackbars(Activity activity) {
         this.mActivity = activity;
@@ -27,21 +26,36 @@ public class Snackbars {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void snackbar(int messageResId) {
-        mSnackbar = Snackbar.make(((ViewGroup) mActivity.findViewById(android.R.id.content)).getChildAt(0), "", Snackbar.LENGTH_SHORT);
+        Snackbar snackbar = Snackbar.make(((ViewGroup) mActivity.findViewById(android.R.id.content)).getChildAt(0), "", Snackbar.LENGTH_SHORT);
         @SuppressLint("InflateParams") View view = mActivity.getLayoutInflater().inflate(R.layout.snackbar_without_action, null);
         MaterialTextView snackbarText = view.findViewById(R.id.textView_snackbar_text);
         snackbarText.setText(messageResId);
         ViewCompat.setElevation(view, 6f);
-        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) mSnackbar.getView();
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
         snackbarLayout.setBackgroundColor(Color.TRANSPARENT);
         snackbarLayout.setPadding(0, 0, 0, 0);
         snackbarLayout.addView(view, 0);
-        mSnackbar.show();
+        snackbar.show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void snackbar(int messageResId, View anchorView) {
+        Snackbar snackbar = Snackbar.make(((ViewGroup) mActivity.findViewById(android.R.id.content)).getChildAt(0), "", Snackbar.LENGTH_SHORT);
+        snackbar.setAnchorView(anchorView);
+        @SuppressLint("InflateParams") View view = mActivity.getLayoutInflater().inflate(R.layout.snackbar_without_action, null);
+        MaterialTextView snackbarText = view.findViewById(R.id.textView_snackbar_text);
+        snackbarText.setText(messageResId);
+        ViewCompat.setElevation(view, 6f);
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+        snackbarLayout.setBackgroundColor(Color.TRANSPARENT);
+        snackbarLayout.setPadding(0, 0, 0, 0);
+        snackbarLayout.addView(view, 0);
+        snackbar.show();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void snackbar(int messageResId, int actionTextResId, View.OnClickListener onClickListener) {
-        mSnackbar = Snackbar.make(((ViewGroup) mActivity.findViewById(android.R.id.content)).getChildAt(0), "", Snackbar.LENGTH_LONG);
+        Snackbar snackbar = Snackbar.make(((ViewGroup) mActivity.findViewById(android.R.id.content)).getChildAt(0), "", Snackbar.LENGTH_LONG);
         @SuppressLint("InflateParams") View view = mActivity.getLayoutInflater().inflate(R.layout.snackbar_with_action, null);
         MaterialTextView snackbarText = view.findViewById(R.id.textView_snackbar_text);
         MaterialButton snackbarActionButton = view.findViewById(R.id.button_snackbar_action);
@@ -49,10 +63,28 @@ public class Snackbars {
         snackbarActionButton.setText(actionTextResId);
         snackbarActionButton.setOnClickListener(onClickListener);
         ViewCompat.setElevation(view, 6f);
-        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) mSnackbar.getView();
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
         snackbarLayout.setBackgroundColor(Color.TRANSPARENT);
         snackbarLayout.setPadding(0, 0, 0, 0);
         snackbarLayout.addView(view, 0);
-        mSnackbar.show();
+        snackbar.show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void snackbar(int messageResId, int actionTextResId, View.OnClickListener onClickListener, View anchorView) {
+        Snackbar snackbar = Snackbar.make(((ViewGroup) mActivity.findViewById(android.R.id.content)).getChildAt(0), "", Snackbar.LENGTH_LONG);
+        snackbar.setAnchorView(anchorView);
+        @SuppressLint("InflateParams") View view = mActivity.getLayoutInflater().inflate(R.layout.snackbar_with_action, null);
+        MaterialTextView snackbarText = view.findViewById(R.id.textView_snackbar_text);
+        MaterialButton snackbarActionButton = view.findViewById(R.id.button_snackbar_action);
+        snackbarText.setText(messageResId);
+        snackbarActionButton.setText(actionTextResId);
+        snackbarActionButton.setOnClickListener(onClickListener);
+        ViewCompat.setElevation(view, 6f);
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+        snackbarLayout.setBackgroundColor(Color.TRANSPARENT);
+        snackbarLayout.setPadding(0, 0, 0, 0);
+        snackbarLayout.addView(view, 0);
+        snackbar.show();
     }
 }
