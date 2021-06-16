b14bec5b49e9eafb0b0a7e9ab8eb9bf771e71683
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-06-16 18:31:32 +0530

    Update extra constants

--- app/src/main/java/net/intensecorp/meeteazy/utils/Extras.java
+++ app/src/main/java/net/intensecorp/meeteazy/utils/Extras.java
@@ -2,18 +2,34 @@ package net.intensecorp.meeteazy.utils;
 
 public class Extras {
     public static final String EXTRA_FIRST_NAME = "net.intensecorp.meeeteazy.utils.extra.FIRST_NAME";
-    public static final String EXTRA_USER = "net.intensecorp.meeeteazy.utils.extra.USER";
+
+    public static final String EXTRA_CONTACT = "net.intensecorp.meeeteazy.utils.extra.CONTACT";
+
     public static final String EXTRA_EMAIL = "net.intensecorp.meeeteazy.utils.extra.EMAIL";
+
     public static final String EXTRA_CALL_TYPE = "net.intensecorp.meeeteazy.utils.extra.CALL_TYPE";
+
     public static final String EXTRA_CALLEE = "net.intensecorp.meeeteazy.utils.extra.CALLEE";
+
     public static final String EXTRA_CALLEES = "net.intensecorp.meeeteazy.utils.extra.CALLEES";
+
     public static final String EXTRA_OTHER_CALLEES_COUNT = "net.intensecorp.meeeteazy.utils.extra.OTHER_CALLEES_COUNT";
+
     public static final String EXTRA_CALLER_FIRST_NAME = "net.intensecorp.meeeteazy.utils.extra.CALLER_FIRST_NAME";
+
     public static final String EXTRA_CALLER_LAST_NAME = "net.intensecorp.meeeteazy.utils.extra.CALLER_LAST_NAME";
+
     public static final String EXTRA_CALLER_EMAIL = "net.intensecorp.meeeteazy.utils.extra.CALLER_EMAIL";
+
     public static final String EXTRA_CALLER_PROFILE_PICTURE_URL = "net.intensecorp.meeeteazy.utils.extra.CALLER_PROFILE_PICTURE_URL";
+
     public static final String EXTRA_CALLER_FCM_TOKEN = "net.intensecorp.meeeteazy.utils.extra.CALLER_FCM_TOKEN";
+
     public static final String EXTRA_RESPONSE_TYPE = "net.intensecorp.meeeteazy.utils.extra.RESPONSE_TYPE";
+
     public static final String EXTRA_REQUEST_TYPE = "net.intensecorp.meeeteazy.utils.extra.REQUEST_TYPE";
+
     public static final String EXTRA_ROOM_ID = "net.intensecorp.meeeteazy.utils.extra.ROOM_ID";
+
+    public static final String EXTRA_IS_SELF = "net.intensecorp.meeeteazy.utils.extra.IS_SELF";
 }
