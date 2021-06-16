eccfa5e85bd91fcd6b374c5b68bb59747c426662
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-06-16 18:58:36 +0530

    Update Settings activity

--- app/src/main/java/net/intensecorp/meeteazy/activities/SettingsActivity.java
+++ app/src/main/java/net/intensecorp/meeteazy/activities/SettingsActivity.java
@@ -1,10 +1,16 @@
 package net.intensecorp.meeteazy.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.appbar.MaterialToolbar;
 
 import net.intensecorp.meeteazy.R;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,5 +18,23 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        MaterialToolbar materialToolbar = findViewById(R.id.materialToolbar_settings);
+        LinearLayout accountLayout = findViewById(R.id.linearLayout_account);
+        LinearLayout themeLayout = findViewById(R.id.linearLayout_theme);
+        setSupportActionBar(materialToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        materialToolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        accountLayout.setOnClickListener(v -> startEditProfileActivity());
+
+        themeLayout.setOnClickListener(v -> {
+            // TODO: Show Set Theme dialog
+        });
+    }
+
+    private void startEditProfileActivity() {
+        Intent editProfileIntent = new Intent(SettingsActivity.this, EditProfileActivity.class);
+        startActivity(editProfileIntent);
     }
 }
